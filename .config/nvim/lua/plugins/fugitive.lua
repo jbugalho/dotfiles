5d46887baba83a2bb99dee533fed906764a62ea9
João Bugalho <joaomnpbugalho@gmail.com>
2024-05-11 11:13:28 +0100

    feat: added dap

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,1 +1,7 @@
-return { "tpope/vim-fugitive" }
+return {
+    "tpope/vim-fugitive",
+    lazy = false,
+	keys = {
+        { "<leader>gs", vim.cmd.Git },
+	}
+}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,5 +4,8 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	keys = {
 		{ "<leader>pf", function() require("telescope.builtin").find_files() end, desc = "Find Files" },
+        { '<leader>pg', function() require("telescope.builtin").live_grep() end, desc = "Live Grep" },
+        { '<leader>pb', function() require("telescope.builtin").buffers() end, desc = "Buffers" },
+        { '<leader>ph', function() require("telescope.builtin").help_tags() end, desc = "Help tags" },
 	}
 }
